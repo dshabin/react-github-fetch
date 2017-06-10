f8f6cce12c9b94bfefbe7e27042656f215f6b4d2
Shabin <d.shabin@gmail.com>
2017-06-10 14:19:53 +0400

    Fetch with username input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
+// styling
 const img_styling = {
   borderTopLeftRadius: '50% 50%',
   borderTopRightRadius: '50% 50%',
@@ -15,6 +15,10 @@ const div_styling = {
   flex : 1,
   flexDirection: 'column',
   justifyContent: 'center',
+}
+
+const hidden_styling = {
+  display : 'none'
 }
 
 const inner_div_styling ={
@@ -36,20 +40,50 @@ const label_styling = {
 }
 
 
+
 class App extends Component {
 
-  state = {followers :0 ,avatar_url : '',public_repos : 0 , name : '-'};
+  //whithout initializing ; the first render will be failed
+  state = {followers :0 ,avatar_url : '',public_repos : 0 , name : '-',text : ''};
 
-  async componentDidMount(){
-    const req = await fetch('http://api.github.com/users/dshabin');
+  changeHandler = (evn) => {
+    this.setState({
+      text: evn.target.value
+    });
+  }
+
+  async doFetch() {
+    const username_to_fetch = this.state.text
+    let req;
+
+    try {
+      req = await fetch('http://api.github.com/users/'+ username_to_fetch);
+    }catch (e){
+      console.log(e.message);
+    }
+
     const {followers,avatar_url,public_repos,name} = await req.json();
     this.setState({followers,avatar_url,public_repos,name})
-    console.log(this.state)
   }
+
+
 
   render() {
     return (
       <div className="App" style={div_styling}>
+        <div>
+        <input
+          placeholder="Enter a username to fetch.."
+          type="text"
+          onChange={this.changeHandler}
+        />
+        <input
+          type="button"
+          value="fetch"
+          onClick= {this.doFetch.bind(this)}
+        />
+        </div>
+
         <div>
           <img alt = "" style = {img_styling} src={this.state.avatar_url} />
         </div>
